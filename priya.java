f8fcbbf75b4e09905ef02f99d90a5954783e72a8
pavankalyan <pavankalyanr125@gmail.com>
2025-02-10 18:25:04 +0530

    i am loving priya

---
+++ priya.java
@@ -0,0 +1,8 @@
+pavan: Hi priya how are you ?
+priya: hi mama, i am fine
+       what about you ?
+pavan: i am also good priya
+       How is for your mother now
+priya: she is very good now mama, she recovered very well
+pavan: ooohh! that's so good
+
